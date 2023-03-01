445d7461b348006b9b38cc770ac459f83c4b0e33
Matheus Henrique <matheus.bezerra@senado.leg.br>
2023-03-01 08:42:49 -0300

    setup do styled-components e fontes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <div className="App">hello world</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div className="App">hello world</div>
+    </ThemeProvider>
+  )
 }
